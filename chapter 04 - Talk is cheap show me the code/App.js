fc0c2d8d3a9576385733b92db553566b0d21dfe5
Dinesh Rout <rddinesh779@gmail.com>
2023-04-15 00:36:08 +0530

    first commit

---
+++ chapter 04 - Talk is cheap show me the code/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * Navbar
+ *   - Logo
+ *   - Nav Items
+ * Body
+ *   - Search Bar
+ *   - Restaurnt Container
+ *       - Restaurnt Card
+ * Footer
+ *   - Copyright
+ */
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello namaste react🚀</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ chapter 04 - Talk is cheap show me the code/README.md
@@ -0,0 +1,18 @@
+# Namste React
+
+## Parcel
+
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- Uses File Watching Algorithm
+- Faster Build (Caching)
+- Image Optimization
+- Minifications
+- Bundling
+- Compress files
+- Code Splitting
+- Differential Bundling (Support for older browsers)
+- Better error suggestions
+- HTTpS support
+- Tree shaking - Remove unused code
